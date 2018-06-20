f16a6519889412d05db9103eceb7b301ae3b192b
Juliana Kang <julianajlk@gmail.com>
2018-06-20 14:00:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == lowercase
     return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
