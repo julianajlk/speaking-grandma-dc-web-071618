79f8ef368cb617b0d6f8ddf49ece12d1f6a06d85
Juliana Kang <julianajlk@gmail.com>
2018-06-20 14:04:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == lowercase
+  if phrase == downcase
     return "HUH?! SPEAK UP, SONNY!"
   else if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
