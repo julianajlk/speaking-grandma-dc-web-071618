20f32b10c04752b53a17b28d7b1a60ed75a595e1
Juliana Kang <julianajlk@gmail.com>
2018-06-20 13:53:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,6 @@ end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
